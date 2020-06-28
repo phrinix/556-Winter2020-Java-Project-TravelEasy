bf8bdbb2c106e3fcbb33b3d8b085bd1a1217a096
phrinix <60997906+phrinix@users.noreply.github.com>
2020-06-28 02:40:35 -0400

    Add files via upload

---
+++ TravelEasy/WebContent/datepicker.js
@@ -0,0 +1,13 @@
+window.onload = function(){
+	new JsDatePick({
+		useMode:2,
+		target:"adate",
+		dateFormat:"%F %d, %Y"
+	});
+	
+	new JsDatePick({
+		useMode:2,
+		target:"ddate",
+		dateFormat:"%F %d, %Y"
+	});
+};
